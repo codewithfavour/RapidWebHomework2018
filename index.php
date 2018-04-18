d84606ef6b6cb1016310a13a29c11543672c7c49
Tobby Junior <32496239+TobbyJay@users.noreply.github.com>
2018-04-18 01:05:55 +0000

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <meta name="author" content="">
 
     <title>Tobby Junior | Resume</title>
-    <link rel="shortcut icon" href="img/FB_IMG_15228095320717875.jpg">
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -24,11 +23,15 @@
     <!-- Custom styles for this template -->
     <link href="css/resume.min.css" rel="stylesheet">
 
+    <style>
+      
+      color:red;
+      </style>
   </head>
 
- <body id="page-top">
+  <body id="page-top">
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top" id="sideNav">
       <a class="navbar-brand js-scroll-trigger" href="#page-top">
         <span class="d-block d-lg-none">Umoh Tobby Jr.</span>
         <span class="d-none d-lg-block">
@@ -41,7 +44,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            <a class="nav-link js-scroll-trigger" href="#about">About Me </a>
           </li>
           <li class="nav-item">
             <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
@@ -66,16 +69,17 @@
 
       <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
         <div class="my-auto">
-          <h1 class="mb-0">Umoh
+          <h1 class="mb-0">Umoh 
             <span class="text-primary">Tobby Jr.</span>
           </h1>
-          <div class="subheading mb-5"> 24, Victory Crescent Estate, Meiran, Lagos. <span> Tel:+2347056507458. </span>
-            <a href="mailto:name@email.com"> umohjunior96@gmail.com</a>
+          <div class="subheading mb-5"> 24, victory crescent , Meiran, Lagos.  <strong> Tel:</strong>+2347056507458
+            <a href="mailto:name@email.com">umohjunior96@gmail.com
+            </a>
           </div>
-          <p class="mb-5"> I'm passionate about building,developing, and designing simple but well sophisticated website/web-apps alike that will be useful to everyone and provide solution to pressing needs.</p>
+          <p class="mb-5">I'm passionate about building,developing, and designing simple but well sophisticated website/web-apps alike that will be useful to everyone and provide solution to pressing needs.</p>
           <ul class="list-inline list-social-icons mb-0">
             <li class="list-inline-item">
-              <a href="#">
+              <a href="  https://web.facebook.com/tobby.junior.397?__nodl">
                 <span class="fa-stack fa-lg">
                   <i class="fa fa-circle fa-stack-2x"></i>
                   <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
@@ -91,7 +95,7 @@
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="#">
+              <a href="  https://www.linkedin.com/in/tobby-junior-26115a13b/">
                 <span class="fa-stack fa-lg">
                   <i class="fa fa-circle fa-stack-2x"></i>
                   <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
@@ -99,7 +103,7 @@
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="#">
+              <a href="https://github.com/TobbyJay">
                 <span class="fa-stack fa-lg">
                   <i class="fa fa-circle fa-stack-2x"></i>
                   <i class="fa fa-github fa-stack-1x fa-inverse"></i>
@@ -117,11 +121,11 @@
           <div class="resume-item d-flex flex-column flex-md-row mb-5">
             <div class="resume-content mr-auto">
               <h3 class="mb-0">Intern - Web Dev. </h3>
-              <div class="subheading mb-3">Prudent Synergy Solutions ltd. </div>
-              <p>Built samples of Web pages for some websites using just html, css under supervision from a senior Web developer </p>
+              <div class="subheading mb-3">Prudent Synergy Solutions Ltd.</div>
+              <p>Built samples of Web pages for some websites using just html, css under supervision from a senior Web developer</p>
             </div>
             <div class="resume-date text-md-right">
-              <span class="text-primary">January 2017 - August 2017</span>
+              <span class="text-primary">January 2017- August 2017</span>
             </div>
           </div>
 
@@ -158,12 +162,12 @@
               <span class="text-primary">March 2018 - Present.</span>
             </div>
           </div>
-           
 
+        </div>
 
       </section>
 
-      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+     <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
         <div class="my-auto">
           <h2 class="mb-5">Education</h2>
 
@@ -200,32 +204,37 @@
           <div class="subheading mb-3">Programming Languages &amp; Tools</div>
           <ul class="list-inline list-icons">
             <li class="list-inline-item">
-              <i class="devicons devicons-html"></i>
+              <i class="devicons devicons-html5"></i>
             </li>
             <li class="list-inline-item">
-              <i class="devicons devicons-css"></i>
+              <i class="devicons devicons-css3"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="devicons devicons-bootstrap"></i>
             </li>
             
-            <li class="list-inline-item">
-              <i class="devicons devicons-c#"></i>
-            </li>
-            <li class="list-inline-item">
-              <i class="devicons devicons-visual studio"></i>
-            </li>
-             <li class="list-inline-item">
-              <i class="devicons devicons-sqlserver"></i>
-            </li>
-            
-          </ul>
 
-          
+            <div class="subheading mb-3">Workflow</div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Mobile-First, Responsive Design</li>
+            <li>
+              
+              
+            
+             
+             
+          </ul>
+        </div>
       </section>
 
       <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
         <div class="my-auto">
           <h2 class="mb-5">Interests</h2>
-          <p>Well, stepping out of my web development or software Dev zone, I enjoy doing other things too, such as </p>
-          <ul>
+          <p>I have always been in love with space exploration that i have always visualized being an astronaut, if given the opportunity i would become one. Reading fantasy novels is one of my hobbies. When outdoor, i would really love to play basketball but i don't have the skills.</p>
+          <p class="mb-0">Well, stepping out of my web development or software Dev zone, I enjoy doing other things too, such as </p>
+            <ul>
           <li> Playing football. </li>
            <li> Hanging out with friends.  </li>
             <li> Playing games (FIFA, Call of duty, God of war). </li>
@@ -237,18 +246,31 @@
           
           
           </ul>
-          
         </div>
       </section>
 
       <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
         <div class="my-auto">
           <h2 class="mb-5">Awards &amp; Certifications</h2>
-          <li>
+          <ul class="fa-ul mb-0">
+            <li>
               <i class="fa-li fa fa-trophy text-warning"></i>
-              IBM Watson Assistant - Chatbot Developer.
-              </li>
+              IBM Watson ChatBot Builder</li>
+            <li>
+
+              
+             
+         
+              
+              
+              
             
+            
+              
+              
+            </ul>
+      
+     
         </div>
       </section>
 
